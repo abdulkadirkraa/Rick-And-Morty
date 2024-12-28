ed1f355817aea825eaa13901fad5930fa0d6a3b0
abdulkadirkraa <karaabdulkadir498@gmail.com>
2024-12-28 04:55:39 +0300

    refactor

--- app/src/main/java/com/abdulkadirkara/rickandmorty/data/datasource/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/abdulkadirkara/rickandmorty/data/datasource/RemoteDataSourceImpl.kt
@@ -10,7 +10,6 @@ import com.abdulkadirkara.rickandmorty.data.remote.service.ApiService
 import com.abdulkadirkara.rickandmorty.di.coroutines.DispatcherType
 import com.abdulkadirkara.rickandmorty.di.coroutines.RickAndMortyDispatchers
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RemoteDataSourceImpl @Inject constructor(
@@ -24,32 +23,32 @@ class RemoteDataSourceImpl @Inject constructor(
         }
     }
 
-    override suspend fun getSingleCharacter(id: Int): NetworkResponse<CharacterResponse> = withContext(ioDispatcher) {
-        return@withContext ioDispatcherCall(ioDispatcher) {
+    override suspend fun getSingleCharacter(id: Int): NetworkResponse<CharacterResponse> {
+        return ioDispatcherCall(ioDispatcher) {
             safeApiCall { apiService.getSingleCharacter(id) }
         }
     }
 
-    override suspend fun getAllLocations(): NetworkResponse<LocationResponse> = withContext(ioDispatcher) {
-        return@withContext ioDispatcherCall(ioDispatcher) {
+    override suspend fun getAllLocations(): NetworkResponse<LocationResponse> {
+        return ioDispatcherCall(ioDispatcher) {
             safeApiCall { apiService.getAllLocations() }
         }
     }
 
-   override suspend fun getSingleLocation(id: Int): NetworkResponse<Result> = withContext(ioDispatcher) {
-        return@withContext ioDispatcherCall(ioDispatcher) {
+   override suspend fun getSingleLocation(id: Int): NetworkResponse<Result> {
+        return ioDispatcherCall(ioDispatcher) {
             safeApiCall { apiService.getSingleLocation(id) }
         }
     }
 
-    override suspend fun getMultipleCharacters(ids: List<Int>): NetworkResponse<List<CharacterResponse>> = withContext(ioDispatcher){
-        return@withContext ioDispatcherCall(ioDispatcher) {
+    override suspend fun getMultipleCharacters(ids: List<Int>): NetworkResponse<List<CharacterResponse>> {
+        return ioDispatcherCall(ioDispatcher) {
             safeApiCall { apiService.getMultipleCharacters(ids) }
         }
     }
 
-    override suspend fun searchCharacter(name: String): NetworkResponse<CharactersResponse> = withContext(ioDispatcher) {
-        return@withContext ioDispatcherCall(ioDispatcher) {
+    override suspend fun searchCharacter(name: String): NetworkResponse<CharactersResponse> {
+        return ioDispatcherCall(ioDispatcher) {
             safeApiCall { apiService.searchCharacter(name) }
         }
     }
